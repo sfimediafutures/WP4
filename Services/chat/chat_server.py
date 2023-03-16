d8ac5d71e9a2e7e23328785b74c159003906855d
Njål Borch <njbo@norceresearch.no>
2023-03-16 13:41:07 +0100

    Fix

--- Services/chat/chat_server.py
+++ Services/chat/chat_server.py
@@ -3,6 +3,8 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import openai
 import json
 
+from google.cloud import texttospeech
+
 
 SYSTEM_CONTEXT=""" You are CouchGPT, a helpful and playful couch companion
 that replies to questions give a certain context. You will answer in a short
@@ -18,13 +20,16 @@ def chat(message, context):
           model="gpt-3.5-turbo",
           messages=[
                 {"role": "system", "content": SYSTEM_CONTEXT},
-                {"role": "user", "content": "This is just context:\n" + context},
+                {"role": "user", "content": "This is just context, first reply with a single line with the language (two letters):\n" + context},
                 {"role": "assistant", "content": "Ok, how can I help??"},
                 {"role": "user", "content": message}
             ]
         )
 
-    return response['choices'][0]['message']['content']
+    text = response['choices'][0]['message']['content']
+    # The first line should be the language
+    lang, response = text.split("\n", 1)
+    return {"lang": lang, "response": response}
 
 
 class RequestHandler(SimpleHTTPRequestHandler):
@@ -42,7 +47,7 @@ class RequestHandler(SimpleHTTPRequestHandler):
             if "message" not in data or "context" not in data:
                 return self.send_error(400, "Bad request")
 
-            response = {"response": chat(data["message"], data["context"])}
+            response = chat(data["message"], data["context"])
             response_json = json.dumps(response).encode("utf-8")
 
             print("Question:", data["message"])

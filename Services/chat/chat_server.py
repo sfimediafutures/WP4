cf6619ff17e06eb8a0a3d93c9183825e5c3e4372
Njål Borch <njbo@norceresearch.no>
2023-03-16 14:23:12 +0100

    Works pretty well, language is still just guesswork

--- Services/chat/chat_server.py
+++ Services/chat/chat_server.py
@@ -10,26 +10,29 @@ SYSTEM_CONTEXT=""" You are CouchGPT, a helpful and playful couch companion
 that replies to questions give a certain context. You will answer in a short
 and conversational way, not elaborating if not particularly asked to do so.
 If you don't know the answer or it's not in the given context, you could try
-with a relevant witty remark or joke. Respond in the same language as the
-user messages, or if you can't detect it, use the language of the context.
-"""
+with a relevant witty remark or joke. """
 
-def chat(message, context):
+def chat(message, context, chatmsgs):
 
-    response = openai.ChatCompletion.create(
-          model="gpt-3.5-turbo",
-          messages=[
+    msgs = [
                 {"role": "system", "content": SYSTEM_CONTEXT},
-                {"role": "user", "content": "This is just context, first reply with a single line with the language (two letters):\n" + context},
+                {"role": "user", "content": "Respond in the same language as the user messages, or if you can't detect it, use the language of the context. This is the context:\n" + context},
                 {"role": "assistant", "content": "Ok, how can I help??"},
                 {"role": "user", "content": message}
             ]
+    for chat in chatmsgs:
+        msgs.append({"role": chat["role"], "content": chat["text"]})
+
+    response = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages=msgs
         )
 
     text = response['choices'][0]['message']['content']
+    print(dir(response))
     # The first line should be the language
-    lang, response = text.split("\n", 1)
-    return {"lang": lang, "response": response}
+    # lang, response = text.split("\n", 1)
+    return {"lang": "en", "response": text}
 
 
 class RequestHandler(SimpleHTTPRequestHandler):
@@ -47,7 +50,12 @@ class RequestHandler(SimpleHTTPRequestHandler):
             if "message" not in data or "context" not in data:
                 return self.send_error(400, "Bad request")
 
-            response = chat(data["message"], data["context"])
+            if "chat" in data:
+                chatmsgs = data["chat"]
+            else:
+                chatmsgs = None
+
+            response = chat(data["message"], data["context"], chatmsgs)
             response_json = json.dumps(response).encode("utf-8")
 
             print("Question:", data["message"])
@@ -58,8 +66,9 @@ class RequestHandler(SimpleHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(response_json)
 
-        except ValueError:
-            self.send_error(400, 'Invalid JSON')
+        except ValueError as e:
+            print("ERROR", e)
+            self.send_error(400, '{"message": "Invalid JSON"}')
 
 PORT = 8987
 
